a1f983e9200af173a3a4c3302a626d4d910ca687
Birgith Jóhansdóttir Christiansen <birgithchristiansen@hotmail.com>
2020-11-03 15:04:05 +0100

    Birgith Frontend
    Index page setup,
    Bootstrap files,
    Style.css
    Preparing for signup page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemon = require("nodemon");
 const app = express();
 const port = 8080;
 app.use(express.static("frontend"));
@@ -6,9 +7,17 @@ app.use(express.static("frontend"));
 //____________________________________________________________________________
 
 app.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/frontend/test.html");
+    return res.sendFile(__dirname + 'frontend/index.html');
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'frontend/test.html')
+})
 
 app.listen(port, () => {
     console.log("Server is running on port:", port)
 });
+
+
+
+nodemon 
