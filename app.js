ee6bd37beab99d96f442d61e1ee3faac0e7645ed
Birgith Jóhansdóttir Christiansen <birgithchristiansen@hotmail.com>
2020-11-04 13:40:47 +0100

    Login, sign up
    Views for login + signup

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ app.use(express.static("frontend"));
 //____________________________________________________________________________
 
 app.get("/", (req, res) => {
-    return res.sendFile(__dirname + 'frontend/index.html');
+    return res.sendFile(__dirname + '/frontend/index.html');
 });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + 'frontend/test.html')
 })
+
+app.get("/", (req, res) => {
+    return res.sendFile(__dirname + '/frontend/loginModal.html');
+});
 
 app.listen(port, () => {
     console.log("Server is running on port:", port)
